bbb4fba985760608ddea055db4bb55216ccb8de8
Yimin2 <gownfla14@naver.com>
2023-11-17 09:26:05 +0900

    refactoring

---
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -0,0 +1,90 @@
+package com.ll.sb20231114.domain.article.article.controller;
+
+import com.ll.sb20231114.domain.article.article.entity.Article;
+import com.ll.sb20231114.domain.article.article.service.ArticleService;
+import com.ll.sb20231114.global.rq.Rq;
+import com.ll.sb20231114.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+    private final Rq rq;
+
+    @GetMapping("/article/write")
+    String showWrite() {
+        return "article/write";
+    }
+
+    @PostMapping("/article/write")
+    @ResponseBody
+    RsData write(
+            String title,
+            String body
+    ) {
+        Article article = articleService.write(title, body);
+
+        RsData<Article> rs = new RsData<>(
+                "S-1",
+                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+                article
+        );
+
+        return rs;
+    }
+
+    @GetMapping("/article/getLastArticle")
+    @ResponseBody
+    Article getLastArticle() {
+        return articleService.findLastArticle();
+    }
+
+    @GetMapping("/article/getArticles")
+    @ResponseBody
+    List<Article> getArticles() {
+        return articleService.findAll();
+    }
+
+    @GetMapping("/article/articleServicePointer")
+    @ResponseBody
+    String articleServicePointer() {
+        return articleService.toString();
+    }
+
+    @GetMapping("/article/httpServletRequestPointer")
+    @ResponseBody
+    String httpServletRequestPointer(HttpServletRequest req) {
+        return req.toString();
+    }
+
+    @GetMapping("/article/httpServletResponsePointer")
+    @ResponseBody
+    String httpServletResponsePointer(HttpServletResponse resp) {
+        return resp.toString();
+    }
+
+    @GetMapping("/article/rqPointer")
+    @ResponseBody
+    String rqPointer() {
+        return rq.toString();
+    }
+
+    @GetMapping("/article/rqTest")
+    String showRqTest(Model model) {
+        String rqToString = rq.toString();
+
+        model.addAttribute("rqToString", rqToString);
+
+        return "article/rqTest";
+    }
+}
---
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -0,0 +1,31 @@
+package com.ll.sb20231114.domain.article.article.repository;
+
+import com.ll.sb20231114.domain.article.article.entity.Article;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleRepository {
+    private final List<Article> articles;
+
+    public Article save(Article article) {
+        if (article.getId() == null) {
+            article.setId(articles.size() + 1L);
+        }
+
+        articles.add(article);
+
+        return article;
+    }
+
+    public Article findLastArticle() {
+        return articles.getLast();
+    }
+
+    public List<Article> findAll() {
+        return articles;
+    }
+}
